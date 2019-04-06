49c7902b8b973116ded93f13d007cb15eb68c897
MaxHogart <irunaradionova@gmail.com>
2019-04-06 11:18:23 +0300

    Add files via upload

--- page/CROWDME/script/script.js
+++ page/CROWDME/script/script.js
@@ -1,20 +1,41 @@
 let burger = document.querySelector(".menu");
 let menu = document.querySelector("ul");
-let btn = document.querySelector('.button');
-let form = document.querySelector('.form');
+let btn = document.querySelectorAll('.button');
+let form1 = document.querySelector('.form');
+let contentForm = document.querySelector('.regestration');
+let closeBtn = document.querySelector('.regestration .close');
+let sendData = document.querySelector(".regestration input[type='submit']");
+let thank= document.querySelector('.thanks');
 
 burger.onclick = function () {
-	// if (menu.style.display == "none") {
-	// 	menu.style.display = "block";
-	// } else {
-	// 	menu.style.display = "none";
-	// }
-
 	menu.classList.toggle('active');
 }
 
-btn.onclick = function () {
-	form.style.display = 'block';
+for (let i=0; i<btn.length; i++) {
+	btn[i].onclick = function (e) {
+		e.preventDefault();
+		form1.classList.add('show');
+		contentForm.classList.add('show');
+	}
 }
-console.log(btn);
 
+function closeForm1() {	
+	form1.classList.remove('show');
+	contentForm.classList.remove('show');
+}
+
+form1.addEventListener('click', closeForm1);
+
+closeBtn.onclick = closeForm1;
+
+console.log(sendData);
+
+sendData.addEventListener('click', function () {
+	closeForm1();
+	setTimeout(function () {
+		thank.classList.add('thanks__show');
+	}, 500);
+	setTimeout(function () {
+		thank.classList.remove('thanks__show');
+	}, 3500)
+})
